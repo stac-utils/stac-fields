b5a038e8a8f64418119be54c764e2de771eac736
Corentin Junod <corentin.junod@outlook.com>
2025-06-24 18:11:39 +0200

    Update datatypes.js
    Co-authored-by: Matthias Mohr <matthias@mohr.ws>

--- datatypes.js
+++ datatypes.js
@@ -27,10 +27,10 @@ const DataTypes = {
 
 	string(str, unit = '') {
 		if(str.startsWith("http://") || str.startsWith("https://")){
-			try {
-				const url = new URL(str);
-				return _.toLink(url.toString(), url.toString())
-			} catch (exception) {}
+			const url = URL.parse(str);
+			if (url) {
+				return _.toLink(url.toString());
+			}
 		}
 		return _.unit(_.e(str).replace(/(\r\n|\r|\n){2,}/g, '<br>'), unit);
 	},
